888ffd92760d23eb4e7a35eda722470e3cccf2e3
ano-miniminiz <anomiya369@gmail.com>
2021-07-02 22:13:57 +0900

    add script gene,del

---
+++ Assets/delete.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class delete : MonoBehaviour
+{
+    public Button btn2;
+    GameObject btn1;
+    generation script;
+
+    void Start()
+    {
+        btn1 = GameObject.Find("Button_gene");
+        script = btn1.GetComponent<generation>();
+    }
+
+    public void OnClick()
+    {
+        btn2.interactable = false;
+        // Destroy(script.obj1);
+        script.btn1.interactable = true;
+    }
+}
---
+++ Assets/generation.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class generation : MonoBehaviour
+{
+    public Button btn1;
+    public GameObject prefab, parentobject, btn2;
+    public GameObject obj1;
+
+    delete script;
+    
+    void Start()
+    {
+        parentobject = GameObject.Find("Canvas");
+        prefab = (GameObject)Resources.Load("Cube");
+        obj1 = Instantiate(prefab) as GameObject;
+        obj1.transform.SetParent(parentobject.transform, false);
+        btn1 = GetComponent<Button>();
+        btn2 = GameObject.Find("Button_del");
+        script = btn2.GetComponent<delete>();
+        btn1.interactable = false;
+    }
+
+    public void Onclick()
+    {
+        btn1.interactable = true;
+        obj1 = Instantiate(prefab) as GameObject;
+        obj1.transform.SetParent(parentobject.transform, false);
+    }
+}
